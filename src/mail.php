f266be5a27479dc28c3a52df0ef8132cfb5c355e
Collin Kleest <collinkleest@gmail.com>
2020-04-07 14:18:29 -0400

    semi finished product, going into production

---
+++ src/mail.php
@@ -0,0 +1,19 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['phone']))
+$email = $_POST['phone'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
+
+$content="From: $name \n Email: $email \n Phone: $phone \n Message: $message";
+
+$recipient = "collinkleest@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email Sent!";
+?>
